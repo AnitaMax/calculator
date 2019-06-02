de7e39268e64dd2cf9c3f513c8fa738e499ab7f1
maxzheng <1836941753@qq.com>
2019-06-02 23:03:19 +0800

    first commit

--- calculter/ViewController.swift
+++ calculter/ViewController.swift
@@ -10,21 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var previous: UILabel!
+
     @IBOutlet weak var main: UILabel!
-    @IBOutlet weak var result: UILabel!
+    @IBOutlet weak var describ: UILabel!
+    @IBOutlet weak var sin: UIButton!
+    @IBOutlet weak var tan: UIButton!
+    @IBOutlet weak var cos: UIButton!
     
     var haspoint = false
     var isopt = false
     var hasleft = false
     var isResult = false
+    var isNaN = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.previous.text = ""
         self.main.text = "0"
-        self.result.text = ""
+        self.describ.text = ""
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -32,11 +35,15 @@ class ViewController: UIViewController {
     }
     
     @IBAction func clear(_ sender: UIButton) {
+        self.describ.text = ""
         self.main.text = "0"
-        self.result.text = ""
         self.haspoint = false
         self.isopt = false
         self.hasleft = false
+        self.isNaN = false
+        self.brain.clearDecscibsion()
+        //恢复变大的结果
+        main.font = UIFont.systemFont(ofSize: 28)
     }
     var curValue:Double{
         get{
@@ -47,10 +54,17 @@ class ViewController: UIViewController {
         }
     }
     @IBAction func numbers(_ sender: UIButton) {
+        //恢复变大的结果
+        main.font = UIFont.systemFont(ofSize: 28)
+        //处理当前显示为上一次的处理结果的场景
         if isResult {
             main.text = "0"
+            haspoint=false
         }
         isResult = false
+        //处理当前显示不合法的场景
+        isNaN = false
+
         switch  sender.currentTitle! {
         case ".":
             if self.haspoint == true {
@@ -77,13 +91,24 @@ class ViewController: UIViewController {
         
     }
     
+    @IBAction func last(_ sender: UIButton) {
+        main.text = String(lastequal)
+        isResult = true
+    }
     
     @IBAction func back(_ sender: UIButton) {
+        //结果不许回退
+        if isResult {
+            return
+        }
+        
         let pretext:String? = self.main.text
         let len=pretext?.characters.count
+        //回退时如果是小数点就重置小数点flag
         if pretext?.substring(from: (pretext?.index((pretext?.endIndex)!, offsetBy: -1))!) == "."{
             self.haspoint = false
         }
+        //回退
         if len! >= 1 {
             self.main.text = self.main.text?.substring(to: (pretext?.index((pretext?.endIndex)!, offsetBy: -1))!)
         }
@@ -92,17 +117,101 @@ class ViewController: UIViewController {
         }
     }
     var brain = calculatorBrain()
+    let magicWord : Dictionary<String,String>=[
+        "520":"520 i love you",
+        "4.1":"4.1 your festival",
+        "521":"521 fffffffffffff",
+    ]
     @IBAction func one_opts(_ sender: UIButton) {
+        //结果变大
+        if sender.currentTitle == "="{
+            main.font = UIFont.systemFont(ofSize: 50)
+            
+        }
+        else{
+            //恢复变大的结果
+            main.font = UIFont.systemFont(ofSize: 28)
+            
+        }
+        //魔术词的计算
+        for word in magicWord.keys{
+            if magicWord[word] == self.main.text{
+                self.main.text = word
+            }
+        }
+        //当结果不合法时禁止继续计算
+        if self.isNaN == true {
+            self.brain.clearDecscibsion()
+            return
+        }
+        //开始计算
         brain.setOperand(self.curValue)
         brain.performOpt(sender.currentTitle!)
         if let res=brain.result  {
-            self.curValue = res
-            if res == 520{
-                self.main.text = "520 我爱你"
+            //处理不合法结果
+            if res.isNaN || res.isInfinite {
+                self.main.text = "超出范围或不合法"
+                isNaN = true
             }
-        }
+            //  处理整数的小数点问题 520.0 －>520 和 结果的小数点
+                
+            
+            else if String(res).substring(from: String(res).index(String(res).endIndex, offsetBy: -2)) == ".0" {
+                self.main.text=String(res).substring(to: String(res).index(String(res).endIndex, offsetBy: -2))
+                self.haspoint=false
+            }
+            else{
+                self.curValue = res
+                self.haspoint=true
+            }
+            //魔力词显示
+            for word in magicWord.keys {
+                if word == main.text  {
+                    self.main.text = magicWord[word]
+                }
+             }
+         }
+        describ.text = brain.descirbsion!
         isResult = true
     }
     
+    @IBAction func changTriFunc(_ sender: UIButton) {
+        if sin.currentTitle == "sin"{
+            sin.setTitle("sin-¹", for: UIControlState.normal)
+            cos.setTitle("cos-¹", for: UIControlState.normal)
+            tan.setTitle("tan-¹", for: UIControlState.normal)
+        }
+        else{
+            sin.setTitle("sin", for: UIControlState.normal)
+            cos.setTitle("cos", for: UIControlState.normal)
+            tan.setTitle("tan", for: UIControlState.normal)
+        }
+    }
+    
+
+    @IBOutlet weak var line1: UIStackView!
+    @IBOutlet weak var line2: UIStackView!
+    @IBOutlet weak var x: UIButton!
+    @IBOutlet weak var genhao: UIButton!
+    @IBOutlet weak var daoshu: UIButton!
+    @IBOutlet weak var pai: UIButton!
+    @IBOutlet weak var e: UIButton!
+    @IBAction func turn(_ sender: UIButton) {
+        let super_opt  :[UIView ] = [line1,line2,x,genhao,daoshu,pai,e]
+        if line1.isHidden == false {
+            for each in super_opt {
+                each .isHidden = true
+            }
+        }
+        else{
+            for each in super_opt {
+                each .isHidden = false
+            }
+        }
+
+        
+        
+        
+    }
 }
 
--- calculter/calculatorBrain.swift
+++ calculter/calculatorBrain.swift
@@ -7,7 +7,11 @@
 //
 
 import Foundation
+//定义阶乘函数
 func jiecheng(_ num:Double)->Double{
+    if num>100{
+        return Double.nan
+    }
     if num <= 1{
         return num
     }
@@ -15,6 +19,7 @@ func jiecheng(_ num:Double)->Double{
         return num*jiecheng(num-1)
     }
 }
+//定义二元运算结构
 struct binaryopt{
     let firstoprend:Double
     let binoptfunc:((Double,Double)->Double)
@@ -22,18 +27,24 @@ struct binaryopt{
         return binoptfunc(firstoprend,sencondoprend)
     }
 }
-
+//储存上一次等号后的得数
+var lastequal:Double = 0
+//运算结构体
 struct calculatorBrain{
     init(){
         
     }
     private var accumluation:Double?
+    public var descirbsion:String?=""
     var result:Double?{
         get{
             return accumluation
         }
     }
     
+    public mutating func clearDecscibsion(){
+        descirbsion = ""
+    }
     private enum Operation{
         case Constant(Double)
         case unaryOperation((Double)->Double)
@@ -55,13 +66,19 @@ struct calculatorBrain{
         "+-":Operation.unaryOperation(-),
         "=":Operation.equals,
         
-        "2nd":Operation.unaryOperation{num in pow(num,2)},
+        "²":Operation.unaryOperation{num in pow(num,2)},
         "^":Operation.binaryOpt(pow),
         "x!":Operation.unaryOperation(jiecheng),
         "1/x":Operation.unaryOperation{num in 1 / num},
         "lg":Operation.unaryOperation(log10),
         "ln":Operation.unaryOperation(log),
         "%":Operation.unaryOperation{num in num/100},
+        
+        "tan-¹":Operation.unaryOperation(atan),
+        "sin-¹":Operation.unaryOperation(asin),
+        "cos-¹":Operation.unaryOperation(acos),
+        
+        "last":Operation.Constant(lastequal),
         
     ]
     var binOptStruct:binaryopt?
@@ -70,20 +87,57 @@ struct calculatorBrain{
             switch opration {
             case .Constant(let value):
                 accumluation = value
+                descirbsion = opt
             case .unaryOperation(let function):
                 if accumluation != nil{
+                    var num:String
+                    if opt == "+-"{
+                        num = String(function(accumluation!))
+                    }
+                    else if opt == "%" {
+                        num = String(accumluation!)+opt
+                    }
+                    else if opt.range(of: "x") != nil {
+                        num = opt.replacingOccurrences(of: "x", with:String(accumluation!))
+                    }
+                    else if opt == "²" {
+                        num = String(accumluation!) + opt
+                    }
+                    else{
+                        num = opt+String(accumluation!)
+                    }
+                    if binOptStruct != nil{
+                        
+                    }
+                    else{
+                        descirbsion = num
+                    }
                     accumluation = function(accumluation!)
                 }
             case .binaryOpt(let function):
                 if accumluation != nil{
                     binOptStruct = binaryopt(firstoprend: accumluation!, binoptfunc: function)
+                    descirbsion = String(accumluation!)+opt
                     accumluation = nil
                 }
             case .equals:
                 if binOptStruct != nil && accumluation != nil {
+                    descirbsion = descirbsion!+String(accumluation!)
                     accumluation = binOptStruct?.perform(with: accumluation!)
+                    descirbsion = descirbsion!+"="+String(accumluation!)
                     binOptStruct = nil
                 }
+                else{
+                    if ((descirbsion?.range(of: "=")) != nil) || descirbsion == ""  {
+                        descirbsion = String(accumluation!)+"="+String(accumluation!)
+                    }
+                    else{
+                        descirbsion = descirbsion!+"="+String(accumluation!)
+                    }
+                }
+                lastequal = accumluation!
+                operations["last"] = Operation.Constant(lastequal)
+
             }
             
             
